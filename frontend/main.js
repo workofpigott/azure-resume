3c4e8c2a900670e6e1c82463df2a9b957d5472b9
Scott Pigott <heirstarr@hotmail.com>
2023-05-16 10:01:36 -0400

    Created Azure function API

--- frontend/main.js
+++ frontend/main.js
@@ -2,16 +2,16 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionAPI = '';
+const functionAPI = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionAPI).then(response => => {
+    fetch(functionAPI).then(response =>  {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
         count = response.count;
-        document.getElementById("counter").innerText = count;
+        document.getElementById("count").innerText = count;
     }).catch(function(error){
         console.log(error);
     });
